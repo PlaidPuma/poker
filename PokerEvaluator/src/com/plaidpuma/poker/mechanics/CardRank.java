848086b302f7a79c622adb1aaa9b6b6ddc836540
Plaid Puma <PlaidPuma@users.noreply.github.com>
2017-03-31 19:44:39 -0400

    Correct version of CardRank

--- PokerEvaluator/src/com/plaidpuma/poker/mechanics/CardRank.java
+++ PokerEvaluator/src/com/plaidpuma/poker/mechanics/CardRank.java
@@ -2,27 +2,31 @@ package com.plaidpuma.poker.mechanics;
 
 public enum CardRank
 {
-    DEUCE	('2', 2),
-    TREY	('3', 3),
-    FOUR	('4', 4),
-    FIVE	('5', 5),
-    SIX		('6', 6),
-    SEVEN	('7', 7),
-    EIGHT	('8', 8),
-    NINE	('9', 9),
-    TEN		('T', 10),
-    JACK	('J', 11),
-    QUEEN	('Q', 12),
-    KING	('K', 13),
-    ACE		('A', 14);
+    TWO     ('2', 2, "deuce", "deuces"),
+    THREE	('3', 3, "trey", "treys"),
+    FOUR	('4', 4, "four", "fours"),
+    FIVE	('5', 5, "five", "fives"),
+    SIX		('6', 6, "six", "sixes"),
+    SEVEN	('7', 7, "seven", "sevens"),
+    EIGHT	('8', 8, "eight", "eights"),
+    NINE	('9', 9, "nine", "nines"),
+    TEN		('T', 10, "ten", "tens"),
+    JACK	('J', 11, "jack", "jacks"),
+    QUEEN	('Q', 12, "queen", "queens"),
+    KING	('K', 13, "king", "kings"),
+    ACE		('A', 14, "ace", "aces");
 
 	private char symbol;
 	private int value;
+	private String nameSingular;
+	private String namePlural;
 
-    private CardRank(char symbol, int value)
+    private CardRank(char symbol, int value, String nameSingular, String namePlural)
     {
         this.symbol = symbol;
         this.value = value;
+        this.nameSingular = nameSingular;
+        this.namePlural = namePlural;
     }
 
     public char getSymbol()
@@ -33,5 +37,15 @@ public enum CardRank
     public int getValue()
     {
         return this.value;
+    }
+
+    public String getNameSingular()
+    {
+        return this.nameSingular;
+    }
+
+    public String getNamePlural()
+    {
+        return this.namePlural;
     }
 }
