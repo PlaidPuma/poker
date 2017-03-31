dc0cc7b66bc0f7d6a7af2166a2d4dba0c8b50e1c
Plaid Puma <PlaidPuma@users.noreply.github.com>
2017-03-31 19:47:58 -0400

    Changed CardRank from tabs to spaces and added Deck as well to test two-file commit

--- PokerEvaluator/src/com/plaidpuma/poker/mechanics/CardRank.java
+++ PokerEvaluator/src/com/plaidpuma/poker/mechanics/CardRank.java
@@ -3,18 +3,18 @@ package com.plaidpuma.poker.mechanics;
 public enum CardRank
 {
     TWO     ('2', 2, "deuce", "deuces"),
-    THREE	('3', 3, "trey", "treys"),
-    FOUR	('4', 4, "four", "fours"),
-    FIVE	('5', 5, "five", "fives"),
-    SIX		('6', 6, "six", "sixes"),
-    SEVEN	('7', 7, "seven", "sevens"),
-    EIGHT	('8', 8, "eight", "eights"),
-    NINE	('9', 9, "nine", "nines"),
-    TEN		('T', 10, "ten", "tens"),
-    JACK	('J', 11, "jack", "jacks"),
-    QUEEN	('Q', 12, "queen", "queens"),
-    KING	('K', 13, "king", "kings"),
-    ACE		('A', 14, "ace", "aces");
+    THREE   ('3', 3, "trey", "treys"),
+    FOUR    ('4', 4, "four", "fours"),
+    FIVE    ('5', 5, "five", "fives"),
+    SIX     ('6', 6, "six", "sixes"),
+    SEVEN   ('7', 7, "seven", "sevens"),
+    EIGHT   ('8', 8, "eight", "eights"),
+    NINE    ('9', 9, "nine", "nines"),
+    TEN     ('T', 10, "ten", "tens"),
+    JACK    ('J', 11, "jack", "jacks"),
+    QUEEN   ('Q', 12, "queen", "queens"),
+    KING    ('K', 13, "king", "kings"),
+    ACE     ('A', 14, "ace", "aces");
 
 	private char symbol;
 	private int value;
