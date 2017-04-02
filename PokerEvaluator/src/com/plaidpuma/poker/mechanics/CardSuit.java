556aa350594514f6029477a14a99b481e1c26695
Plaid Puma <PlaidPuma@users.noreply.github.com>
2017-04-01 20:09:10 -0400

    Minor changes, mostly testing ability to push to old branch while working in new

--- PokerEvaluator/src/com/plaidpuma/poker/mechanics/CardSuit.java
+++ PokerEvaluator/src/com/plaidpuma/poker/mechanics/CardSuit.java
@@ -1,16 +1,25 @@
 package com.plaidpuma.poker.mechanics;
 
 public enum CardSuit {
-	SPADES     ("spades"),
-	HEARTS     ("hearts"),
-	DIAMONDS   ("diamonds"),
-	CLUBS      ("clubs");
+    //@formatter:off
+	SPADES     ('s', "spades"),
+	HEARTS     ('h', "hearts"),
+	DIAMONDS   ('d', "diamonds"),
+	CLUBS      ('c', "clubs");
+    //@formatter:on
 
+    private char symbol;
     private String name;
 
-    private CardSuit(String name)
+    private CardSuit(char symbol, String name)
     {
+        this.symbol = symbol;
         this.name = name;
+    }
+
+    public char getSymbol()
+    {
+        return symbol;
     }
 
     public String getName()
